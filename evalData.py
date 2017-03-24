15425566240694ee505ef21546065631492d06d8
dschick <schick.daniel@gmail.com>
2017-03-24 11:08:41 +0100

    add new child class scopeTraces
    reading scope traces is now easily possible

--- evalData.py
+++ evalData.py
@@ -1711,3 +1711,91 @@ class pilatusXPP(areaDetector):
 
         return frames
 
+        
+########################################
+        
+class scopeTraces(spec):
+    """Inherit from spec and add capabilities to read and plot scope traces.
+    
+    Attributes:
+        inherited from spec
+        
+    """
+    
+    # properties    
+    scopeDataPath = ''
+    
+    def __init__(self, name, filePath, specFileExt=''):
+        super().__init__(name, filePath, specFileExt)
+        # set the path to the scope raw traces
+        self.scopeDataPath = self.filePath + '/scope/' + self.specFileName + '_{:04d}/F1_' + self.specFileName + '_{:04d}_{:05d}.txt'
+        
+    def readScopeTrace(self, scanNum, scanPoint):
+        # add automatic file copying in the future
+        # import os#,shutil
+        # basePath = 'D:/HZB/Beamtimes/ZPM/2017-02-Schick/scope/'
+        # filepath = 'Dy_{:04d}/F1_Dy_{:04d}_{:05d}.txt'.format(ScanNr,ScanNr,ScanPoint)
+        
+        #     if not os.path.exists(basePath + filepath):
+        #         # copy data if path not exists
+        #         shutil.copytree('/mnt/lecroy/2017/2017-02-Schick/Dy_{:04d}'.format(ScanNr), 'D:/HZB/Beamtimes/ZPM/2017-02-Schick/scope/Dy_{:04d}'.format(ScanNr))
+                
+        trace = genfromtxt(self.scopeDataPath.format(scanNum,scanNum,scanPoint),skip_header=5,delimiter=',')
+        delays = trace[:,0]*1e9 
+        ampl = trace[:,1]
+        
+        return delays, ampl 
+        
+    def readScopeScan(self, scanNum, delayGrid= []):    
+    
+        # check if there is data in the hdf5 file available
+        intensities = self.readDataFromHDF5(scanNum, 'ScopeRaw', 'intensities')
+        
+        delays, _ = self.readScopeTrace(scanNum, 0)
+        
+        if any(intensities):
+            # if the data is present in the HDF5 file and we don't want to 
+            # overwrite, read also the other datasets
+            delays   = self.readDataFromHDF5(scanNum, 'ScopeRaw', 'delays')
+            # print('Scan #{0:.0f} read from HDF5.'.format(scanNum))
+           
+        elif any(delays):
+            # data is not present in the HDF5 file but there are frames
+            # on the disk, so read them and save them
+    
+            # print('Scan #{0:.0f} read from files and saved to HDF5.'.format(scanNum))  
+            
+            folderName = os.path.dirname(self.scopeDataPath.format(scanNum,scanNum,0))
+            
+            numFiles = len(os.listdir(folderName))
+            
+            if len(delayGrid) > 0:
+                delayGrid, _, _, _, _, _, _, _, _ = evalData.binData(delays,delays,delayGrid)        
+                intensities = zeros([numFiles, len(delayGrid)])
+            else:
+                intensities = zeros([numFiles, len(delays)])
+    
+    
+            for i in range(numFiles):
+                _, ampl = self.readScopeTrace(scanNum, i)
+    
+                if len(delayGrid) > 0:
+                    ampl, X, Yerr, Xerr, Ystd, Xstd, edges, bins, n = evalData.binData(ampl,delays,delayGrid,statistic='mean')
+    
+                intensities[i,:] = ampl
+    
+            self.writeData2HDF5(scanNum, 'ScopeRaw', intensities  , 'intensities')
+            self.writeData2HDF5(scanNum, 'ScopeRaw', delays, 'delays')
+    
+        else:
+            # no scope data for this scan
+            print('Scan #{0:.0f} includes no scope data!'.format(scanNum))
+            intensities = []
+        
+        
+        scanData = self.plotScans([scanNum], skipPlot=True)
+        
+        if len(delayGrid) > 0:
+            return scanData, delayGrid, intensities
+        else:
+            return scanData, delays, intensities
