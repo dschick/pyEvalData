67907001993f15870346b44566994774df003fe8
Daniel Schick <schick.daniel@gmail.com>
2020-12-15 15:27:04 +0100

    add license header and fix flake8

--- pyEvalData/helpers.py
+++ pyEvalData/helpers.py
@@ -1,19 +1,26 @@
-# This file is part of the evalData module.
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# The MIT License (MIT)
+# Copyright (c) 2015-2020 Daniel Schick
 #
-# eval Data is free software; you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation; either version 2 of the License, or
-# (at your option) any later version.
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
 #
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
 #
-# You should have received a copy of the GNU General Public License
-# along with this program; if not, see <http://www.gnu.org/licenses/>.
-#
-# Copyright (C) 2015 Daniel Schick <schick.daniel@gmail.com>
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+# IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+# DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+# OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE
+# OR OTHER DEALINGS IN THE SOFTWARE.
 
 import numpy as np
 from scipy.stats import binned_statistic
@@ -34,12 +41,10 @@ def binData(y, x, X, statistic='mean'):
     y = y.flatten('F')
     x = x.flatten('F')
     X = np.sort(X.flatten('F'))
-    
+
     # set non-finite values to 0
     idx = ~np.isfinite(y)
     y[idx] = 0
-   
-
     # create bins for the grid
     edges, _ = edges4grid(X)
 
@@ -72,7 +77,7 @@ def binData(y, x, X, statistic='mean'):
 
         Xstd, _, _ = binned_statistic(x, x, 'std', edges)
         Xerr = Xstd/np.sqrt(n)
-    
+
     # remove NaNs
     Y = Y[n > 0]
     X = X[n > 0]
