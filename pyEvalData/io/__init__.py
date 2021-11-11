3a24c9fb1c1e6419fbf544c07303be537d2e9233
Daniel Schick <schick@mbi-berlin.de>
2021-11-11 10:44:11 +0100

    add sardana_nexus to init

--- pyEvalData/io/__init__.py
+++ pyEvalData/io/__init__.py
@@ -1,6 +1,7 @@
 from .source import Source
 from .scan import Scan
 from .spec import Spec
+from .sardana_nexus import SardanaNeXus
 from .palxfel import PalSpec
 
-__all__ = ['Source', 'Scan', 'Spec', 'PalSpec']
+__all__ = ['Source', 'Scan', 'Spec', 'SardanaNeXus', 'PalSpec']
