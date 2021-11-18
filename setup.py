e846b823c6b9548f50cd27728a87da126b31382a
Daniel Schick <schick@mbi-berlin.de>
2021-11-18 13:07:31 +0100

    Bump version: 1.2.1 → 1.2.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pyEvalData',
-    version='1.2.1',
+    version='1.2.2',
     packages=find_packages(),
     url='https://github.com/dschick/pyEvalData',
     install_requires=['numpy',
