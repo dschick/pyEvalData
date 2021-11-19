27ce74ce30aaee167aa22320e6f049b6bb6ea0b3
EmCeBeh <borchert@mbi-berlin.de>
2021-11-19 22:36:21 +0100

    Parametrised pytest fixtures

--- test/conftest.py
+++ test/conftest.py
@@ -5,18 +5,32 @@ import pyEvalData as ped
 
 
 @pytest.fixture(scope='module')
-def source(tmp_path_factory):
+def source_pal(tmp_path_factory):
     temp_folder = tmp_path_factory.mktemp("pytest_data")
-    print(temp_folder)
-    source = ped.io.Spec(file_name='test_file_spec.spec',
-                         file_path='test/test_data/',
-                         use_nexus=True,
-                         force_overwrite=False,
-                         update_before_read=False,
-                         read_and_forget=True,
-                         nexus_file_path=temp_folder,
-                         nexus_file_name='test')
-    return source
+    source_pal = ped.io.PalH5(name='pal_file',
+                              file_name='{0:07d}',
+                              file_path='example_data/example_files_pal',
+                              use_nexus=True,
+                              force_overwrite=False,
+                              update_before_read=False,
+                              read_and_forget=True,
+                              nexus_file_path=temp_folder,
+                              nexus_file_name='test_pal')
+    return source_pal
+
+
+@pytest.fixture(scope='module')
+def source_spec(tmp_path_factory):
+    temp_folder = tmp_path_factory.mktemp("pytest_data")
+    source_spec = ped.io.Spec(file_name='example_file_spec.spec',
+                              file_path='example_data/',
+                              use_nexus=True,
+                              force_overwrite=False,
+                              update_before_read=False,
+                              read_and_forget=True,
+                              nexus_file_path=temp_folder,
+                              nexus_file_name='test_spec')
+    return source_spec
 
 
 @pytest.fixture(scope='module')
