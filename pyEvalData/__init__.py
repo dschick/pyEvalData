fda8e08bf743bf9e6450bd74cd1c4fc4fb068927
Daniel Schick <schick@mbi-berlin.de>
2021-11-19 14:49:08 +0100

    Bump version: 1.3.3 → 1.3.4

--- pyEvalData/__init__.py
+++ pyEvalData/__init__.py
@@ -10,4 +10,4 @@ from . import io
 
 __all__ = ['Evaluation', 'io']
 
-__version__ = '1.3.3'
+__version__ = '1.3.4'
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pyEvalData',
-    version='1.3.3',
+    version='1.3.4',
     packages=find_packages(),
     url='https://github.com/dschick/pyEvalData',
     install_requires=['numpy',
