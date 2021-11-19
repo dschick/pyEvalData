e1c5b07004b14e0389a980e46506dccc39f44b3d
EmCeBeh <martin.b@robothek.de>
2021-11-19 01:52:19 +0100

    moved fixtures into their own conftest file and extended the test to include evaluation,too

--- test/test_source_spec.py
+++ test/test_source_spec.py
@@ -1,25 +1,24 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 import pytest
-import pyEvalData as ped
-
-
-@pytest.fixture(scope='session')
-def source(tmp_path_factory):
-    temp_folder = tmp_path_factory.mktemp("pytest_data")
-    print(temp_folder)
-    source = ped.io.Spec(file_name='test_file_spec.spec',
-                         file_path='test/test_data/',
-                         use_nexus=True,
-                         force_overwrite=False,
-                         update_before_read=False,
-                         read_and_forget=True,
-                         nexus_file_path=temp_folder,
-                         nexus_file_name='test')
-    return source
 
 
 def test_source(source):
     assert source.name == 'test_file_spec.spec'
     assert source.scan1.meta['number'] == 1
     assert source.scan1.data['delay'][0] == pytest.approx(-0.998557475)
+
+
+def test_evaluation(evaluation):
+    data = evaluation
+    assert data.cdef == {}
+    assert data.clist == []
+    cdef = {
+        'pumped_demod':  'pumped-(unpumped-mean(unpumped))',
+        'pumpedM_demod': 'pumpedM-(unpumpedM-mean(unpumpedM))',
+        'M':             'pumped_demod-pumpedM_demod',
+    }
+    data.cdef = cdef
+    data.clist = ['M']
+    data1 = data.get_scan_data(1)
+    assert data1['delay'][0] == pytest.approx(-0.998557475)
