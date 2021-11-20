40268911ec413db9ea6e6e2f030a981d0d6f2a93
EmCeBeh <borchert@mbi-berlin.de>
2021-11-20 01:16:30 +0100

    fix flake8

--- test/test_evaluation.py
+++ test/test_evaluation.py
@@ -17,8 +17,5 @@ def test_evaluation(evaluation):
     data.clist = ['M']
     data1 = data.get_scan_data(1)
     assert data1['delay'][0] == pytest.approx(-0.998557475)
-    
     y, x, yerr, xerr, name = data.plot_scans([1])
-    
     assert y[data.clist[0]][0] == pytest.approx(0.02183873769)
-    
