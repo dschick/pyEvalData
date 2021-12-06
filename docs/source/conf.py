fa72dad280803cbacbf42ef38580dc7fc79993c5
Daniel Schick <schick@mbi-berlin.de>
2021-12-06 14:34:25 +0100

    run examples locally at rtd

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -60,7 +60,7 @@ templates_path = ['_templates']
 source_suffix = ['.rst', '.md']
 
 jupyter_execute_notebooks = "auto"
-execution_excludepatterns = ['*.ipynb']
+# execution_excludepatterns = ['*.ipynb']
 
 suppress_warnings = ['myst.mathjax']
 
